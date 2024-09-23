0bd71060adf7e4b6feeb0e6b35f149406b305f68
vitor-sld <vitor.cteles@gmail.com>
2024-09-23 15:51:39 -0300

    adicionando variáveis e modificando styles gerais

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -40,7 +40,7 @@ export const Botao = styled.button`
   padding: 8px 12px;
   border: none;
   cursor: pointer;
-  background-color: #2f3640;
+  background-color: ${variaveis.azulEscuro};
   border-radius: 8px;
   margin-right: 8px;
 `
